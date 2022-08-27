1fd987083ed87c5c6eba39228d529850dc259aff
FiodorG <gre_dorelus@yahoo.com>
2022-08-27 12:42:29 -0400

    Downloaded the dependecies

--- src/test/java/extraSessionSaturday/Main.java
+++ src/test/java/extraSessionSaturday/Main.java
@@ -1,5 +1,7 @@
 package extraSessionSaturday;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 public class Main {
 
     public static void main(String[] args) {
