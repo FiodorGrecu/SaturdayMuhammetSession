12b1af56821565facef1e5ca83f9aa859d3c0506
FiodorG <gre_dorelus@yahoo.com>
2022-09-19 23:00:42 -0400

    Connecting to Youtube

--- src/test/java/extraSessionSaturday/Main.java
+++ src/test/java/extraSessionSaturday/Main.java
@@ -4,11 +4,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
 
         String url = "https://www.youtube.com/";
-
+        System.setProperty("webdriver.chrome.driver", "/Users/Work/Desktop/B7-Selenium/chromedriver");
 
         WebDriver driver = new ChromeDriver();
+        driver.get(url);
+
+        Thread.sleep(3000);
+        driver.quit();
     }
 }
